1ae2337974b1006542327437fabd913ae487ce90
Mohamed-Arshad-K <mohamedarshad@pinnacleinfotech.com>
2025-01-29 19:02:40 +0530

    Add project files.

---
+++ SimpleMVC/Controller/NameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using SimpleMVC.Model;
+
+namespace SimpleMVC.Controller
+{
+    public class NameController
+    {
+        private NameModel _model;
+
+        public NameController()
+        {
+            _model = new NameModel();
+        }
+
+        public void DisplayFullName(TextBox fNameTextBox, TextBox sNameTextBox)
+        {
+            _model.FirstName= fNameTextBox.Text;
+            _model.SecondName = sNameTextBox.Text;
+
+            ///Display Message
+            Display(_model);    
+        }
+
+        private void Display(NameModel model)
+        {
+            MessageBox.Show($"{model.FirstName} {model.SecondName}");
+        }
+
+    }
+}
